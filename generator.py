a4edae34931eecd3785f06e49b4bbf1593434fa9
Wuba <sukeshram5@gmail.com>
2022-03-15 11:37:24 +0530

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,50 +1,67 @@
-for mirror_pad import pad
+import numpy as np
+import cv2
+import random 
+from patchify import patchify ,unpatchify
+from sklearn.utils import shuffle
 
-def datagen():
-    lis=[]
+def data_generator(img_dir:list ,input_size :tuple ,batch_size :int ,pad = False)  -> Union[np.ndarray ,np.ndarray]  :
+    r''' Arg :
+         img_dir :Image Directory Path for both Input image and labeled image in list format.
+         
+         input_size : Input Size of the Image in Tuple format. 
+         
+         batch_size : Batch Size.
+         
+         pad : If pad is True, the mirror padding will happen.
+         
+         example : datagen(img_dir_path =[Input_image_path ,Output_image_path] ,input_size = (256,256,3) ,batch_size = 30 ,pad =False)
+         
+         Return :
+         Xtrain , ytrain : Batch of Images return in array
+         
+         
+    '''
+    # PAtch the Images
+    def patch(Image:np.ndarray ,row ,col ):
+        x_train = patchify(Image ,(row ,col ,3) ,step = row) .reshape(total_slice ,row ,col,3) /255 
+        y_train = patchify((labeled_image) ,(row ,col ,3) ,step =row) .reshape(total_slice ,row ,col,3) /255  # change the pat
+    
+    
     num =0
     while True:
-        for i in glob.glob('*.tif'):lis.append(i) # change the format of the photo here I'm using tif format
-        random.shuffle(lis)
-
-        for pic in lis:
+        # List the Input Images Name
+        data_points =[]
+        for i in os.listdir(img_dir_path[0]):data_points.append(i) 
+        data_points = shuffle(data_points)
+        
+        for pic in data_points:
+            
             x_train =[] ;y_train = []
-
-            x_train = patchify(cv2.imread(pic) ,(254 ,254 ,3) ,step =254) .reshape(361 ,254 ,254,3) /255 
-
-            y_train = patchify(cv2.imread(f'/content/change the path /ytrain/{pic}') ,(254 ,254 ,3) ,step =254) .reshape(361 ,254 ,254,3) /255  # change the path 
-
-        for _ in range(2):y_train = np.delete(y_train ,1 ,axis =-1) 
-      
-        temp =0
-        for j in range(31,370 ,30):
-
-            padding = pad(x_train[temp:j] ,y_train[temp:j],2)
-            yield padding[0] ,padding[1]
-            temp=j
-        
-            num+=1
-            print(end = '=')
-        print(f"------{num} Batches Completed -----")
-        
-
-
-def valgen():
-    lis=[]
-    while True:
-        for i in glob.glob('/content/change the path/val_x/*.tif'):lis.append(i) # change the path
-        random.shuffle(lis)
-    
-        for pic in lis:
-            x_val =[] ;y_val = []
-
-            x_val = (patchify(cv2.imread(pic) ,(256 ,256 ,3) ,step =256)).reshape(361,256,256,3) /255
-
-            y_val = (patchify(cv2.imread(pic[0:31]+'val_y'+pic[36:]) ,(512 ,512 ,3) ,step =512)).reshape(361,512,512,3) /255
-
-            for _ in range(2):y_val = np.delete(y_val ,1 ,axis =-1)
+            if pad is True: pixel =2
+            else: pixel = 0
+                
+            # Set the row and column of the images   
+            row = input_size[0]-pixel
+            col = input_size[1]-pixel
+            
+            Image = cv2.imread(pic) 
+            labeled_image = cv2.imread(cv2.imread(img_dir_path[1]+f'{pic}') 
+            total_slice = int((image.shape[0])/row)**2                     
+                                      
+            x_train ,y_train = patch(Image ,row ,col )
+                                       
+            # Change the dimension into One i.e (256,256,3) <- (256,256,1)                            
+            for _ in range(2):y_train = np.delete(y_train ,1 ,axis =-1) 
             temp =0
 
-            for j in range(31,361 ,30):
-                yield x_val[temp:j] ,y_val[temp:j]
-                temp=j
+            for j in range(batch_size+1 ,total_slice ,batch_size):
+
+                padding = pad(x_train[temp:j] ,y_train[temp:j],pixel)
+                x ,y =shuffle(padding[0] ,padding[1])        
+                                       
+                yield x,y
+                temp=j ;num+=1
+                print(end = '=')
+            print(f"------{num} Batches Completed -----")
+
+        
